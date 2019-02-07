986b07253075e327349208a0dd75cd2864ffacbf
Sofia Cheung <659@holbertonschool.com>
2019-02-07 22:56:05 +0000

    assign a random num to a variable and add conditions depending on the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ else if (last < 6 && n != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
-else if (last == 0)
+else 
 {
 printf("Last digit of %d is %d and is 0\n", n, last);
 }
