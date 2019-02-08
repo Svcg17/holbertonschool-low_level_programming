f18296889a250778fd4c7341dfd4d411f9b1b831
Sofia Cheung <659@holbertonschool.com>
2019-02-08 00:13:25 +0000

    print all digit numbers base 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+B#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = '0';
+while (c <= '9')
+{
+putchar(c);
+c = c + 1;
+}
+printf("\n");
+return (0);
+}
