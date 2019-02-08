724b977f80006d98c62c4fefa15eed650632ab90
Sofia Cheung <659@holbertonschool.com>
2019-02-08 03:23:55 +0000

    print nums from 00 to 99

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
- #include<stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,18 +6,16 @@
  */
 int main(void)
 {
-int c = '0';
-while (c <= '9')
+int c;
+for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
+if (c != '9')
+{
 putchar(',');
 putchar(' ');
-if (c == '9')
-{
-putchar (c);
 }
-c = c + 1;
 }
-printf("\n");
+putchar('\n');
 return (0);
 }
