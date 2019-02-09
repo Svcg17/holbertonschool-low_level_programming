4d2aa0f3f8e6b13417a559041f609fdee7dde08e
Sofia Cheung <659@holbertonschool.com>
2019-02-09 06:24:14 +0000

    advanced task print a string without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include<unistd.h>
 #include<stdio.h>
 /**
  * main - Entry point
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-int c;
 write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,1", 58);
 putchar('\n');
+return (0);
 }
