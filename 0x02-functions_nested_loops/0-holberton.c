18538d156a6c2163ba8033e152c871f59faddc65
Sofia Cheung <659@holbertonschool.com>
2019-02-11 23:56:19 +0000

    Write a program that prints Holberton, followed by a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c[] = "Holberton";
+int i = 0;
+while (c[i] != '\0')
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
