5bafb87e12d8896ee3674e330aebe94d418e3051
Sofia Cheung <659@holbertonschool.com>
2019-02-14 20:58:31 +0000

    Write a function that draws a diagonal line on the terminal.

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal.
+ * @n: number of times the character \ should be printed.
+ * Return: 0.
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n == 0 || n < 0)
+{
+_putchar('\n');
+}
+for (i = 1; i <= n; i++)
+{
+for (j = 0; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
