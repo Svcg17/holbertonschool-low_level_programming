111e7c5ee20682a7393537ef7b6ecb4bc5ce8211
Sofia Cheung <659@holbertonschool.com>
2019-02-15 04:11:20 +0000

    Write a function that prints a triangle, followed by a new line.

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,21 @@
  */
 void print_triangle(int size)
 {
-int a,b;
+int a, b;
 if (size == 0 || size < 0)
 {
 _putchar('\n');
 }
 for (a = 1; a <= size; a++)
 {
-      for (b = 1; b < size; b++)
-	{
-      _putchar(' ');
+for (b = a; b < size; b++)
+{
+_putchar(' ');
 }
-      for (b = 1; b <= a; b++)
-	{    
-	  _putchar('#');
-	}
-      _putchar('\n');
+for (b = 1; b <= a; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
 }
 }
