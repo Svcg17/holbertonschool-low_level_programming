a3dd0b37d03e9e9dc06fb852610d236b4cd40bd8
Sofia Cheung <659@holbertonschool.com>
2019-02-15 00:37:48 +0000

    10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that prints a triangle.
+ * @size: size of the triangle.
+ * Return: 0.
+ */
+void print_triangle(int size)
+{
+int a,b;
+if (size == 0 || size < 0)
+{
+_putchar('\n');
+}
+for (a = 1; a <= size; a++)
+{
+      for (b = 1; b < size; b++)
+	{
+      _putchar(' ');
+}
+      for (b = 1; b <= a; b++)
+	{    
+	  _putchar('#');
+	}
+      _putchar('\n');
+}
+}
