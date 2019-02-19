88a8a9e74eba55b0cc7ee8f5ea126ba3d65792a9
Sofia Cheung <659@holbertonschool.com>
2019-02-19 19:12:05 +0000

    function that prints a string, in reverse, followed by a new line

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,22 @@
 #include "holberton.h"
 /**
- * print_rev - print a string in reverse and a newline.
- * @s: pointer to a char for the string.
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a char for the length.
+ * Return: i.
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+}
+/**
+ * print_rev - print a string in reverse.
+ * @s: pointer to a char.
  */
 void print_rev(char *s)
 {
