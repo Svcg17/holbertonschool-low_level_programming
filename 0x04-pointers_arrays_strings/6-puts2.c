749663e88db1f38d1addc204af43381619f00880
Sofia Cheung <659@holbertonschool.com>
2019-02-19 20:25:37 +0000

    function that prints one char out of 2 of a string, followed by a new line

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a char for the length.
+ * Return: i.
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+}
+/**
+ * puts2 - print one char out of 2 of a string.
+ * @str: pointer to a char to print.
+ */
+void puts2(char *str)
+{
+  int i = 0;
+  int len;
+
+  len = _strlen(str);
+  for(i = 0; i <= len - 1; i+=2)
+  {
+	  _putchar(str[i]);
+  }
+  _putchar('\n');
+}
