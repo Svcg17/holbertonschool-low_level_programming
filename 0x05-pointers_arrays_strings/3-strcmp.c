2fd063ff4aebf1a887c9b7d8d981ddabc5161f26
Sofia Cheung <659@holbertonschool.com>
2019-02-21 23:05:45 +0000

    function that compares two strings

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: pointer to a char.
+ * @s2: pointer to a char
+ * Return: depending on the comparison.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i])
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
