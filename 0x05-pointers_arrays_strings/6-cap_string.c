2a84faa4bfde9c960e14d44b03c3e3e84988e524
Sofia Cheung <659@holbertonschool.com>
2019-02-22 07:17:07 +0000

    function that capitalizes all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a char for the length.
+ * Return: i.
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+}
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @s: pointer to a char.
+ * Return: i.
+ */
+char *cap_string(char *s)
+{
+        int i = 0;
+
+        for (i = 0; s[i]; i++)
+	{
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+			{
+				s[i] = s[i] - 32;
+			}
+			continue;
+		}
+		if ((s[i] == ' ') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			++i;
+			if(s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+		}
+	}
+        return (s);
+}
