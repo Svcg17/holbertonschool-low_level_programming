1316adc446fd65e218306fddf9fb1916b13bbeea
Sofia Cheung <659@holbertonschool.com>
2019-02-22 00:50:33 +0000

    function that changes all lowercase letters of a string to uppercase

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to a char for the length.
+ * Return: i.
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+}
+/**
+ * string_toupper - function that changes all lowercase to uppercase.
+ * @s: pointer to a char.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	int len = _strlen(s);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
