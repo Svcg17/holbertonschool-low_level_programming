15ed995e5a2728393070a08cfe3e764593f0093e
Sofia Cheung <659@holbertonschool.com>
2019-03-02 22:03:40 +0000

    program that prints the number of arguments passed into it

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments.
+ * @argv: array of pointers to the arguments entered.
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
