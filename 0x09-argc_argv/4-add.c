dfaec77223f60f0b8fbf2a0b4ce7a89ace389279
Sofia Cheung <659@holbertonschool.com>
2019-03-03 06:33:42 +0000

    program that adds positive numbers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,7 +1,22 @@
 #include "holberton.h"
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * _isdigit - checks for a digit from 0 through 9.
+ * @c: integer taken to check the conditions.
+ * Return: 1 is c is digit.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 /**
  * main - Entry point
  * @argc: number of arguments.
@@ -20,14 +35,16 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(*argv[i]))
+		if (_isdigit(*argv[i]))
+		{
+	  	        sum = sum + atoi(argv[i]);
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-	        sum = sum + atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
