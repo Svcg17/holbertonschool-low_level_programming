95a640b79783eff302b5b88ca0672711e138b56c
Sofia Cheung <659@holbertonschool.com>
2019-03-03 06:17:06 +0000

    program that adds positive numbers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,25 +12,24 @@ int main(int argc, char *argv[])
 {
 	int i = 0, sum = 0;
 
-	if (argc < 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (argc < 1)
 		{
-			if (isdigit(*argv[i]))
-			{
-				sum = sum + atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("0\n");
+		}
+
+		if (isdigit(*argv[i]))
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }
