5480aa28c6278765ca9aae37e8c9de45b67d7a63
Sofia Cheung <659@holbertonschool.com>
2019-03-05 22:11:36 +0000

    function that creates an array of chars, and initializes it with a specific char

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -14,6 +14,22 @@ int _strlen(char *s)
 	return (i);
 }
 /**
+ * _strcpy - function that copies the string pointed to by src.
+ * @dest: pointer to a char.
+ * @src: pointer to a char.
+ * Return: the pointer to dest (c).
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+/**
  * _strdup - returns a pointer to a newly allocated space in memory
  * containing a copy of the string given as parameter.
  * @str: string to make a copy of.
@@ -23,22 +39,15 @@ int _strlen(char *s)
 char *_strdup(char *str)
 {
 	char *s;
-	int i;
 	int len = _strlen(str);
-
-	s = malloc (sizeof(char) * len + 1);
 
 	if (str == '\0')
 		return ('\0');
 
-	if (str == '\0')
-		return('\0');
+	s = malloc(len + 1);
+	if (s == '\0')
+		return ('\0');
 
-	for (i = 0; str[i]; i++)
-	{
-		s[i] = str[i];
-	}
-	s[i] = '\0';
+	_strcpy(s, str);
 	return (s);
-	free(s);
 }
