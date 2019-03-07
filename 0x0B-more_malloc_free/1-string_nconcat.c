4dd011e381db3c2dcfa8571bdde2b48483162f76
Sofia Cheung <659@holbertonschool.com>
2019-03-07 22:44:38 +0000

    function that concatenates two strings

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,39 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _strlen - function that returns the length of a string.
- * @s: pointer to a char for the length.
- * Return: i.
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
-/**
- * string_nconcat - function that concatenates two strings.
- * @s1: string.
- * @s2: string to concatenate.
- * @n: n amount of s2 to concatenate.
- * Return: pointer to newlly allocated space in memory with the concatenated
- * string, if fails return null.
+ * string_nconcat - function that concatenates two strings
+ * @s1: first string.
+ * @n: size of string 2 to concatenate.
+ * @s2: string to add to first one.
+ * Return: pointer to a newly allocated space in memory, null terminated.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	unsigned int j = 0, len;
-	char *array, *c;
-
-	len = (_strlen(s1) + n);
+	char *array;
+	int i, a, c;
+	unsigned int j, b;
 
 	if (s1 == '\0')
 		s1 = "";
 	if (s2 == '\0')
 		s2 = "";
 
-	array = malloc(sizeof(char) * len + 1);
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+
+	if (j > n)
+		j = n;
+
+	array = malloc(sizeof(char) * (i + j + 1));
 
 	if (array == '\0')
 		return ('\0');
-
-	if ((int)n >=  _strlen(s2))
-		n = _strlen(s2);
-
-	for (i = 0; s1[i]; i++)
-		array[i] = s1[i];
-
-	c = array;
-	while (array[j])
-		array++;
-	for (j = 0; s2[j] && j < n; j++)
-		array[j] = s2[j];
-	return (c);
+	for (a = 0; a < i; a++)
+		array[c++] = s1[a];
+	for (b = 0; b < j; b++)
+		array[c++] = s2[b];
+	array[c] = '\0';
+	return (array);
 }
