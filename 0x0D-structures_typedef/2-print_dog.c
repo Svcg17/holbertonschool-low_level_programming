cd3a2f21805c9a739c9c7e48b739d6cc3ecaa325
Sofia Cheung <659@holbertonschool.com>
2019-03-11 21:06:27 +0000

    added newline to nil

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@ void print_dog(struct dog *d)
 	if (d == '\0')
 		return;
 	if (d->name == '\0' || d->age == '\0' || d->owner == '\0')
-		printf("(nil)");
+		printf("(nil)\n");
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
