6c735f7ed4dea7f971173092ba8597ea7b4c08ac
Sofia Cheung <659@holbertonschool.com>
2019-03-11 22:00:15 +0000

    changed first if condition

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -6,23 +6,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != '\0')
-	{
-		if (d->name != '\0')
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-
-		if (d->age == d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-
-		if (d->owner != '\0')
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
-	}
-	else
+	if (d == '\0')
 		return;
+
+	if (d->name != '\0')
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+       	if (d->age)
+		printf("Age: %.6f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+
+	if (d->owner != '\0')
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
 }
