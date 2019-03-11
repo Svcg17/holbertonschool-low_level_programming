d492b6b12748b77f52de5e04eb2001723e59d70b
Sofia Cheung <659@holbertonschool.com>
2019-03-11 21:11:20 +0000

    made an if statement for when the struct contents are not null

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,7 +10,11 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == '\0' || d->age == '\0' || d->owner == '\0')
 		printf("(nil)\n");
+	if (d->name != '\0' && d->age != '\0' && (d->age == d->age)
+&& d->owner != '\0')
+	{
 	printf("Name: %s\n", d->name);
 	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
+	}
 }
