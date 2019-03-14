47fbaa5173fe4795ee4a2a1da3f1aadeb1dcf21a
Sofia Cheung <659@holbertonschool.com>
2019-03-14 04:00:10 +0000

    function to get the right operator from 3-op_functions

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * get_op_func - function that selects the correct function to perform the
+ * operation asked by the user.
+ * @s: operator passed as argument to the program.
+ * Return: pointer to the function that corresponds to the operator given.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+/* if position i in ops content == the char in s, if the char in s in only one*/
+/* return the pointer to the func matching it*/
+		if (ops[i].op[0] == *s)
+			return(ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
