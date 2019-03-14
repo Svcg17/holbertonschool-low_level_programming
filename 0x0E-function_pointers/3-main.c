14ec8e563604a19b430b0f96e2d885eb41d1be80
Sofia Cheung <659@holbertonschool.com>
2019-03-14 03:58:14 +0000

    main function

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of parameters.
+ * @argv: arguments passed from command line.
+ * Return: 0 in success.
+ */
+int main(int argc, char *argv[])
+{
+	int (*function)(int, int);
+	int total;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == '\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+/*getting the right pointer to the function of the op we need*/
+	function = get_op_func(argv[2]);
+	if (function == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+/*get op from get_op_func*/
+	total = function(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", total);
+	return (0);
+}
