e4a5304d7df2973c55fcb941b94ca3a130c97cce
Sofia Cheung <659@holbertonschool.com>
2019-03-15 06:15:03 +0000

    added case condition and the way get_op_func is called inside function by printf

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,36 +1,31 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - main function
+ * main - main function that handles validation inputs.
  * @argc: number of parameters.
  * @argv: arguments passed from command line.
  * Return: 0 in success.
  */
 int main(int argc, char *argv[])
 {
-	int (*function)(int, int);
-	int total;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == '\0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-/*getting the right pointer to the function of the op we need*/
-	function = get_op_func(argv[2]);
-	if (function == NULL)
+	if (!get_op_func(argv[2]))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-/*get op from get_op_func*/
-	total = function(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", total);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
