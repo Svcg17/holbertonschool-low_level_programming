c222b709b534d4d4a0285791ef249f755d01fc0a
Sofia Cheung <659@holbertonschool.com>
2019-03-16 20:02:12 +0000

    fixed betty errors

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,30 +1,45 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+/**
+ * _charp - print character
+ * @list: argument character.
+ */
 void _charp(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
-
+/**
+ * _intp - print and int.
+ * @list: argument integer.
+ */
 void _intp(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
-
+/**
+ * _floatp - print a float.
+ * @list: argument float.
+ */
 void _floatp(va_list list)
 {
 	printf("%f", va_arg(list, double));
 }
+/**
+ * _stringp - print a string
+ * @list: argument string.
+ */
 void _stringp(va_list list)
 {
 	char *s;
-	s = va_arg(list, char*);
-	if (s != NULL)
-		printf("%s", s);
-	else
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+	{
 		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
 }
-
-
 /**
  * print_all - function that prints anything.
  * @format: list of arguments passed to the function.
@@ -43,14 +58,15 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	char *sepa = "";
+
 	va_start(list, format);
 
-	while(format[i])
+	while (format != NULL && format[i])
 	{
 		j = 0;
 		while (types[j].type)
 		{
-			if(format[i] == *types[j].type)
+			if (format[i] == *types[j].type)
 			{
 				printf("%s", sepa);
 				types[j].func(list);
@@ -62,5 +78,4 @@ void print_all(const char * const format, ...)
 	}
 	printf("\n");
 	va_end(list);
-
 }
