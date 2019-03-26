87fe11613597cd517567ad99b5e387d04971f68f
Sofia Cheung <659@holbertonschool.com>
2019-03-26 19:28:56 +0000

    Add if statement for when head is null to return null, return newnode in when index is at zero

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (newnode == NULL)
 		return (NULL);
+	if (head == NULL)
+		return (NULL);
 
 	newnode->n = n;
 	if (idx == 0)
 	{
 		newnode->next = *head;
 		*head = newnode;
+		return (newnode);
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
