c8feebd9764a5cf739d6087c8497727d4ca74bcc
Sofia Cheung <659@holbertonschool.com>
2019-03-26 18:15:39 +0000

    Add condition for when head is null to return

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *t;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		t = *head;
 		*head = (*head)->next;
-		free (t);
+		free(t);
 	}
 }
