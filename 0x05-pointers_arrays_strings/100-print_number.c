37d8efc1f02af1354f91e5555b1758979859cfa8
Sofia Cheung <659@holbertonschool.com>
2019-04-04 00:38:11 +0000

    Print an integer

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_number - function that prints an integer.
+ * @n: integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -(m);
+	}
+	if (m / 10 != 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar(m % 10 + '0');
+}
