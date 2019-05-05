5f0951caef6feb86388286197c535ee971f5f761
Sofia Cheung <659@holbertonschool.com>
2019-05-05 19:05:30 +0000

    Debugging when deleting first node resulting in an empty list

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,26 +13,34 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL || head == NULL)
 		return (-1);
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(current);
-		return (1);
-	}
-	for (i = 0; i <	index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (current->next != NULL)
 			current = current->next;
 		else
-		{
-			current->prev->next = NULL;
-			free(current);
-			return (1);
-		}
+			return (-1);
+	}
+	if (index == 0 && current == *head && (*head)->next == NULL)
+	{
+		*head = NULL;
+		free(current);
+		return (1);
+	}
+	if (index == 0 && current == *head)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	if (current->next == NULL)
+	{
+		current->prev->next = NULL;
+		free(current);
+		return (1);
 	}
 	current->prev->next = current->next;
 	current->next->prev = current->prev;
 	free(current);
-	current = current->next;
 	return (1);
 }
