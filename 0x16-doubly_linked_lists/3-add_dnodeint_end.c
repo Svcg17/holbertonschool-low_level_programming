97ae0f6c23bc6886842aa90df95495a9033beba6
Sofia Cheung <659@holbertonschool.com>
2019-05-05 01:08:39 +0000

    Added condition if malloc fails

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,15 +12,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *current = *head;
 
+	if (head == NULL || new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->prev = NULL;
 	new->next = NULL;
 
-	if (head == NULL)
-		return (NULL);
 	if (*head == NULL)
 	{
-		new->prev = NULL;
 		*head = new;
 		return (new);
 	}
