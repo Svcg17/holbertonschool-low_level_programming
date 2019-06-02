642e245fdc504f0248f15a9ebef5bdd56cd2a622
Sofia Cheung <659@holbertonschool.com>
2019-06-02 21:10:49 +0000

    Free hash table if allocating in array fails

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,7 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	hasht->array = calloc(size, sizeof(hash_node_t));
 	if (hasht->array == NULL)
+	{
+		free(hasht);
 		return (NULL);
+	}
 	hasht->size = size;
 	return (hasht);
 }
