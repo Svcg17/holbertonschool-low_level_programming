9864d0364be442c08576178a6a9dc98cff1f33f1
Sofia Cheung <659@holbertonschool.com>
2019-07-25 01:41:07 +0000

    not calling function

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -8,18 +8,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = binary_tree_node(parent, value);
-	binary_tree_t *t = NULL;
+	binary_tree_t *newnode = NULL;
 
+	newnode = malloc(sizeof(binary_tree_t));
 	if (parent == NULL || newnode == NULL)
 		return (NULL);
 
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->parent = parent;
+
 	if (parent->left != NULL)
 	{
-		t = parent->left;
-		parent->left = newnode;
-		newnode->left = t;
-		t->parent = newnode;
+		parent->left->parent = newnode;
+		newnode->left = parent->left;
 	}
 	parent->left = newnode;
 
