c9bb84d736b384b52e26af8399fe54ae94cca93d
Sofia Cheung <659@holbertonschool.com>
2019-07-25 05:00:20 +0000

    Changed to use height function

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: the height of the binary tree, or 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	if (tree == NULL)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree.
  * @tree: pointer to the root node of the tree to measure
  * the balance factor
@@ -15,7 +33,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
 	return(height_l - height_r);
 }
