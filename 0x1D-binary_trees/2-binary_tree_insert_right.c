5f149448737abb028afb4e304532b123e2ad24a3
Sofia Cheung <659@holbertonschool.com>
2019-07-25 03:16:29 +0000

    Check parent before malloc

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -9,9 +9,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode;
 
-	if (parent == NULL || newnode == NULL)
+	if (parent == NULL)
+		return (NULL);
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
