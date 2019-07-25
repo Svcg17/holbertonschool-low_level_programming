bd14706943b665313579dbf4636289d6e1629f13
Sofia Cheung <659@holbertonschool.com>
2019-07-25 23:50:25 +0000

    Removed check in if condition

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->left && node->parent->parent->right)
+	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
 	else
 		return (node->parent->parent->left);
