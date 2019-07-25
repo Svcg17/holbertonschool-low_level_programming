95aa2e04b04d9b173623b16ee764e53449ea7f58
Sofia Cheung <659@holbertonschool.com>
2019-07-25 22:51:10 +0000

    Use depth function instead of height

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,24 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree.
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: the height of the binary tree, or 0.
+ * binary_tree_depth -  measures the depth of a node in a binary tree.
+ * @tree: pointer to the node to measure the depth.
+ * Return: the depth of node or 0.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
+	const binary_tree_t *current;
+	size_t depth;
 
 	if (tree == NULL)
 		return (0);
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	current = tree;
+	depth = 0;
+	while (current->parent)
+	{
+		current = current->parent;
+		depth++;
+	}
+	return (depth);
+	/* return (tree && tree->parent ? 1 + binary_tree_depth(tree->parent) : 0); */
 }
-
-
 /**
  * binary_tree_is_perfect - function that checks if a binary tree
  * is perfect.
