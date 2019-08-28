ab238d73102cff9f2081de9222d1724a85f0d447
Svcg17 <sofiavictoria17@gmail.com>
2019-08-28 20:27:02 +0000

    change else

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -28,13 +28,9 @@ int binary_search(int *array, size_t size, int value)
 		}
 		mid = (left + right) / 2;
 		if (array[mid] < value)
-		{
 			left = mid + 1;
-		}
 		else if (array[mid] > value)
-		{
 			right = mid - 1;
-		}
 		else
 			return (mid);
 	}
